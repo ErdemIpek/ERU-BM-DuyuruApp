274bc1f070dba82d663550900f8dbaf8cd805484
Erdem İpek <ei.erdemipek@gmail.com>
2020-11-18 11:27:06 +0300

    Update announcement_scrapper.py

--- Scrapper/announcement_scrapper.py
+++ Scrapper/announcement_scrapper.py
@@ -2,11 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 from urllib.request import urlopen
 import pymysql
-import time
 import pymysql.cursors
-from flask import Flask,jsonify
-
-start_time = time.time()
 
 #This function is for correcting the fetched texts.
 def strip_func(list):
@@ -18,7 +14,7 @@ def strip_func(list):
         list = ["İçeriği görüntülemek için tıklayınız."]
     return(list)
 #Database connection
-connection = pymysql.connect(host="localhost",user="root",passwd="ztR3hdTj5AEa9SE8",database="my_database",port=3306)
+connection = pymysql.connect(host="localhost")
 cursor = connection.cursor()
 
 #Table truncating
@@ -155,28 +151,3 @@ connection.commit()
 connection.close()
 
 
-def get_data(type):
-    cur.execute("""SELECT * FROM duyuru_db WHERE type = '%s'""" % (type))
-    rows = cur.fetchall()
-    return(rows)
-
-
-con = pymysql.connect(host="localhost",user="root",passwd="ztR3hdTj5AEa9SE8",database="my_database",port=3306,charset='utf8',cursorclass=pymysql.cursors.DictCursor)
-cur = con.cursor()
-
-app= Flask(__name__)
-
-@app.route('/bil_muh')
-def bil_muh_page():
-    return(jsonify(get_data("bil_muh")))
-
-@app.route('/muh_fak')
-def muh_fak_page():
-    return(jsonify(get_data("muh_fak")))
-
-@app.route('/obisis')
-def obisis_page():
-    return(jsonify(get_data("obisis")))
-
-if __name__ == '__main__':
-    app.run(host= '0.0.0.0')
